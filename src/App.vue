8596e22d9aa5a1ee499f2695fef7a856e9b61ff3
metal-face <captain@pirateradio.boats>
2022-11-25 00:48:36 -0500

    Nav Drawer functionality added

--- src/App.vue
+++ src/App.vue
@@ -1,32 +1,54 @@
 <template>
-  <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </div>
-    <router-view/>
-  </div>
+  <v-app>
+    <v-app-bar app dark >
+      <v-app-bar-nav-icon  @click="drawer = !drawer" />
+    </v-app-bar>
+    <v-navigation-drawer v-model="drawer" app dark >
+      <v-list>
+        <v-list-item v-for="(page, i) in pages" :key="i" link>
+          <v-list-item-icon>
+            <v-icon>
+              {{ page.icon }}
+            </v-icon>
+          </v-list-item-icon>
+          <v-list-item-content>
+            <v-list-item-title>
+              {{ page.name }}
+            </v-list-item-title>
+          </v-list-item-content>
+        </v-list-item>
+      </v-list>
+    </v-navigation-drawer>
+
+    <v-main>
+      <v-container fluid fill-height>
+        <router-view />
+      </v-container>
+    </v-main>
+  </v-app>
 </template>
+
+<script>
+export default {
+  components: {},
+  data() {
+    return {
+      drawer: false,
+      pages: [
+        { name: "Home", icon: "mdi-home"},
+        { name: "Login & Signup", icon: "mdi-account" },
+        { name: "Admin", icon: "mdi-security"},
+        { name: "Messenger", icon: "mdi-message"}
+      ],
+    }
+  },
+}
+</script>
 
 <style lang="scss">
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
 }
 
-#nav {
-  padding: 30px;
-
-  a {
-    font-weight: bold;
-    color: #2c3e50;
-
-    &.router-link-exact-active {
-      color: #42b983;
-    }
-  }
-}
 </style>
