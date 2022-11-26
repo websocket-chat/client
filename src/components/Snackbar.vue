ab5d96f2411d49e3bcd4a2c734f0f6b72716ced7
metal-face <captain@pirateradio.boats>
2022-11-26 02:00:32 -0500

    Snackbar created to display server response indicators to the user.

---
+++ src/components/Snackbar.vue
@@ -0,0 +1,35 @@
+<template>
+  <v-snackbar :value="visible" :timeout="timeout" :color="color">
+    {{ text }}
+  </v-snackbar>
+</template>
+
+<script>
+export default {
+  name: "Snackbar",
+  data() {
+    return {
+      timeout: 10000,
+    };
+  },
+  props: {
+    visible: {
+      type: Boolean,
+      required: true,
+      default: false,
+    },
+    text: {
+      type: String,
+      required: true,
+      default: "",
+    },
+    color: {
+      type: String,
+      required: false,
+      default: "success",
+    },
+  },
+};
+</script>
+
+<style scoped></style>
