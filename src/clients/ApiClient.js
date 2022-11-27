1787bdaaa1bb7fe0a8115705bc5db72e56126cec
metal-face <captain@pirateradio.boats>
2022-11-27 01:55:09 -0500

    API client encapsulated into its own client layer.

---
+++ src/clients/ApiClient.js
@@ -0,0 +1,12 @@
+import axios from "axios";
+
+const apiClient = axios.create({
+    baseURL: "https://chat.cmyui.xyz",
+    withCredentials: false,
+    headers: {
+        Accept: "application/json",
+        "Content-Type": "application/json",
+    },
+});
+
+export default apiClient;
