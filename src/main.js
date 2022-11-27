643ac1144a8a5d5cf7265bca805b7c134e30fffd
metal-face <captain@pirateradio.boats>
2022-11-26 22:28:50 -0500

    Vuelidate validation library imported to project as a plugin for global use.

--- src/main.js
+++ src/main.js
@@ -1,13 +1,14 @@
-import Vue from 'vue'
-import './plugins/axios'
-import App from './App.vue'
-import vuetify from './plugins/vuetify'
-import router from './router'
+import Vue from "vue";
+import App from "./App.vue";
+import vuetify from "./plugins/vuetify";
+import vuelidate from "./plugins/vuelidate";
+import router from "./router";
 
-Vue.config.productionTip = false
+Vue.config.productionTip = false;
 
 new Vue({
   vuetify,
+  vuelidate,
   router,
-  render: h => h(App)
-}).$mount('#app')
+  render: (h) => h(App),
+}).$mount("#app");
