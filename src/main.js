906ff71059143414083e42cd1522d05d68addaa7
metal-face <captain@pirateradio.boats>
2022-11-27 17:05:17 -0500

    VueX State management library added to project so that the user information can be retained globally.

--- src/main.js
+++ src/main.js
@@ -3,6 +3,7 @@ import App from "./App.vue";
 import vuetify from "./plugins/vuetify";
 import vuelidate from "./plugins/vuelidate";
 import router from "./router";
+import store from './store'
 
 Vue.config.productionTip = false;
 
@@ -10,5 +11,6 @@ new Vue({
   vuetify,
   vuelidate,
   router,
-  render: (h) => h(App),
+  store,
+  render: (h) => h(App)
 }).$mount("#app");
