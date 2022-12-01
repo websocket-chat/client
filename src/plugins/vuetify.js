7b5edeb2fb0f8d58feef28612a4011abfa386015
metal-face <bhughes@evertz.com>
2022-12-01 01:15:33 -0500

    Light and dark mode written.

--- src/plugins/vuetify.js
+++ src/plugins/vuetify.js
@@ -1,7 +1,31 @@
-import Vue from 'vue';
-import Vuetify from 'vuetify/lib/framework';
+import Vue from "vue";
+import Vuetify from "vuetify/lib/framework";
+import colors from "vuetify/lib/util/colors";
 
 Vue.use(Vuetify);
 
 export default new Vuetify({
+  theme: {
+  dark: true,
+    themes: {
+      light: {
+        primary: colors.purple.lighten1,
+        secondary: colors.purple.lighten2,
+        accent: colors.purple.accent,
+        error: colors.error,
+        info: colors.info,
+        success: colors.success,
+        warning: colors.warning,
+      },
+      dark: {
+        primary: colors.purple.darken1,
+        secondary: colors.purple.darken2,
+        accent: colors.purple.accent,
+        error: colors.error,
+        info: colors.info,
+        success: colors.success,
+        warning: colors.warning,
+      },
+    },
+  },
 });
