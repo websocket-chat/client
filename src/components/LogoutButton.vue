3e05d34a1be4e1197cae0956cb51677a247cc5ed
metal-face <bhughes@evertz.com>
2022-12-04 04:37:17 -0500

    The logout button's functionality was written.
    The user data is captured from localStorage and parsed
    into JSON.
    
    The session ID from the JSON retrieved is passed into the
    logout method.
    
    On successful logout the user is redirected to the
    login and signup page.

--- src/components/LogoutButton.vue
+++ src/components/LogoutButton.vue
@@ -1,13 +1,39 @@
 <template>
-
+  <v-btn v-if="authenticated" x-small @click="logout" color="red">
+    Logout
+  </v-btn>
 </template>
 
 <script>
+import SessionService from "@/services/SessionService";
+
 export default {
-  name: "LogoutButton"
+  name: "LogoutButton",
+  methods: {
+    logout() {
+      let user = JSON.parse(window.localStorage.getItem("user"));
+      SessionService.logout(user.session_id)
+          .catch((err) => {
+            if (err) {
+              this.$emit("snackbar", {
+                text: "Something went wrong logging you out...",
+                visible: true,
+              })
+            }
+          })
+          .then((res) => {
+            if (!res) return;
+            window.localStorage.clear();
+            this.$store.dispatch("user/setUserData", {});
+            this.$store.dispatch("user/setAuthenticated", false);
+            this.$router.push({name: "LoginAndSignUp"});
+          });
+    }
+  },
+  computed: {
+    authenticated() {
+      return this.$store.getters["user/checkAuthentication"];
+    }
+  }
 }
 </script>
-
-<style scoped>
-
-</style>
