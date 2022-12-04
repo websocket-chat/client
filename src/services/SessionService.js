a94ca5cf2a8dda2a1d36edc851dffe79b95920b2
metal-face <bhughes@evertz.com>
2022-12-04 04:38:04 -0500

    Logout method added to service layer.

--- src/services/SessionService.js
+++ src/services/SessionService.js
@@ -1,14 +1,23 @@
 import ApiClient from "@/clients/ApiClient";
 
 export default {
-  login(username, password) {
-    return ApiClient({
-      method: "POST",
-      url: `/v1/sessions`,
-      data: {
-        username: username,
-        password: password,
-      },
-    });
-  },
+    login(username, password) {
+        return ApiClient({
+            method: "POST",
+            url: `/v1/sessions`,
+            data: {
+                username: username,
+                password: password,
+            },
+        });
+    },
+    logout(session_id) {
+        return ApiClient({
+            method: "DELETE",
+            url: "/v1/sessions",
+            headers: {
+                "Authorization": `Bearer ${session_id}`
+            }
+        });
+    }
 };
