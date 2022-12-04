62030d591214206cb2687e701d47c1504a827e68
metal-face <bhughes@evertz.com>
2022-12-04 00:53:44 -0500

    Messenger boiler plate added.

---
+++ src/views/Messenger.vue
@@ -0,0 +1,17 @@
+<template>
+  <v-card height="80%" width="80%" color="primary">
+
+  </v-card>
+</template>
+
+<script>
+export default {
+  name: "Messenger",
+
+
+}
+</script>
+
+<style scoped>
+
+</style>
