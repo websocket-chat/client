96c8435c9c16f41237e0dcce3dc8593bec26abbb
metal-face <bhughes@evertz.com>
2022-12-05 04:48:24 -0500

    User profile card imported

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,15 +1,29 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-  </div>
+  <v-card height="100%" width="100%" class="d-flex justify-center align-center" flat tile>
+    <CircleLoader color="purple" :loading="loading" />
+    <UserProfileCard @loading="toggleLoadingState"/>
+  </v-card>
 </template>
 
 <script>
-// @ is an alias to /src
+import UserProfileCard from "@/components/UserProfileCard";
+import CircleLoader from "@/components/CircleLoader";
 
 export default {
   name: 'Home',
   components: {
+    UserProfileCard,
+    CircleLoader
+  },
+  data() {
+    return {
+      loading: false,
+    }
+  },
+  methods: {
+    toggleLoadingState(state) {
+      this.loading = state;
+    },
   }
 }
 </script>
