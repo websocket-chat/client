8ec6da5bc472629178cea5ff37ee3ba579e92164
metal-face <bhughes@evertz.com>
2022-12-05 04:49:19 -0500

    mobile support added

--- src/components/LoginForm.vue
+++ src/components/LoginForm.vue
@@ -1,36 +1,32 @@
 <template>
-  <v-card flat height="auto" width="90%" class="mx-auto">
-    <form class="">
+  <v-card flat :height="mobile ? '100%' : 'auto'" :width="formWidth" class="mx-auto">
+    <form>
       <v-container fluid fill-height>
         <v-row dense no-gutters>
           <v-col cols="12">
             <v-text-field
-              v-model="username"
-              :error-messages="usernameErrors"
-              solo
-              outlined
-              append-icon="mdi-account"
-              label="Username"
-              class="form__input"
-              clearable
-              clear-icon="mdi-close-circle-outline"
-              @input="$v.username.$touch()"
-              @blur="$v.username.$touch()"
+                v-model="username"
+                :error-messages="usernameErrors"
+                append-icon="mdi-account"
+                label="Username"
+                class="form__input"
+                clearable
+                clear-icon="mdi-close-circle-outline"
+                @input="$v.username.$touch()"
+                @blur="$v.username.$touch()"
             />
           </v-col>
           <v-col cols="12">
             <v-text-field
-              v-model="password"
-              :error-messages="passwordErrors"
-              solo
-              outlined
-              class="form__input"
-              label="Password"
-              append-icon="mdi-lock"
-              clearable
-              clear-icon="mdi-close-circle-outline"
-              @input="$v.password.$touch()"
-              @blur="$v.password.$touch()"
+                v-model="password"
+                :error-messages="passwordErrors"
+                class="form__input"
+                label="Password"
+                append-icon="mdi-lock"
+                clearable
+                clear-icon="mdi-close-circle-outline"
+                @input="$v.password.$touch()"
+                @blur="$v.password.$touch()"
             />
           </v-col>
           <v-col cols="12"></v-col>
@@ -49,7 +45,7 @@
 </template>
 
 <script>
-import { required } from "vuelidate/lib/validators";
+import {required} from "vuelidate/lib/validators";
 import SessionService from "@/services/SessionService";
 
 export default {
@@ -88,32 +84,32 @@ export default {
       this.toggleLoadingState(true);
       this.$v.$touch();
       SessionService.login(this.username, this.password)
-        .catch((error) => {
-          const snackbarText = this.determineSnackbarMessage(
-            error.response.data.error
-          );
-          this.$emit("snackbar", {
-            text: snackbarText,
-            color: "error",
+          .catch((error) => {
+            const snackbarText = this.determineSnackbarMessage(
+                error.response.data.error
+            );
+            this.$emit("snackbar", {
+              text: snackbarText,
+              color: "error",
+            });
+          })
+          .then((res) => {
+            if (!res) return;
+
+            this.$store.dispatch("user/setUserData", res.data.data);
+            this.$store.dispatch("user/setAuthenticated", true);
+
+            window.localStorage.setItem("user", JSON.stringify(res.data.data));
+
+            this.$emit("snackbar", {
+              text: "Success! 🎉",
+              color: "success",
+            });
+          })
+          .finally(() => {
+            this.toggleLoadingState(false);
+            this.$router.push({name: "Home"});
           });
-        })
-        .then((res) => {
-          if (!res) return;
-
-          this.$store.dispatch("user/setUserData",  res.data.data);
-          this.$store.dispatch("user/setAuthenticated", true);
-
-          window.localStorage.setItem("user", JSON.stringify(res.data.data));
-
-          this.$emit("snackbar", {
-            text: "Success! 🎉",
-            color: "success",
-          });
-        })
-        .finally(() => {
-          this.toggleLoadingState(false);
-          this.$router.push({ name: "Messenger" });
-        });
     },
     clearLoginForm() {
       this.username = "";
@@ -122,6 +118,28 @@ export default {
     },
   },
   computed: {
+    mobile() {
+      return this.$vuetify.breakpoint.mobile;
+    },
+    formWidth() {
+      if (this.$vuetify.breakpoint.mobile) {
+        return "100%";
+      }
+      switch (this.$vuetify.breakpoint.name) {
+        case "xl":
+          return "60%";
+        case "lg":
+          return "65%";
+        case "md":
+          return "70%";
+        case "sm":
+          return "80%";
+        case "xs":
+          return "100%";
+        default:
+          return "65%";
+      }
+    },
     usernameErrors() {
       const errors = [];
 
--- src/views/LoginAndSignUp.vue
+++ src/views/LoginAndSignUp.vue
@@ -1,37 +1,37 @@
 <template>
   <v-card height="100%" width="100%" flat tile>
-    <CircleLoader color="primary" :loading="loading" />
+    <CircleLoader color="primary" :loading="loading"/>
     <v-card
-      height="100%"
-      width="100%"
-      class="d-flex justify-center align-center"
+        height="100%"
+        width="100%"
+        class="d-flex justify-center align-center"
     >
-      <v-card height="auto" width="50%" outlined elevation="5">
+      <v-card flat :height="mobile ? '50%' : 'auto'" :width="mobile ? '100%' : '50%'"  >
         <v-tabs v-model="tab" centered>
-          <v-tabs-slider color="accent" />
-          <v-tab> Signup </v-tab>
-          <v-tab> Login </v-tab>
+          <v-tabs-slider color="accent"/>
+          <v-tab> Signup</v-tab>
+          <v-tab> Login</v-tab>
         </v-tabs>
         <v-tabs-items v-model="tab">
           <v-tab-item>
             <SignupForm
-              @snackbar="handleSnackbarEvent"
-              @loading="toggleLoadingState"
+                @snackbar="handleSnackbarEvent"
+                @loading="toggleLoadingState"
             />
           </v-tab-item>
           <v-tab-item>
             <LoginForm
-              @snackbar="handleSnackbarEvent"
-              @loading="toggleLoadingState"
+                @snackbar="handleSnackbarEvent"
+                @loading="toggleLoadingState"
             />
           </v-tab-item>
         </v-tabs-items>
       </v-card>
       <v-snackbar
-        v-model="snackbar"
-        :timeout="3000"
-        :color="snackbarColor"
-        class="text-center"
+          v-model="snackbar"
+          :timeout="3000"
+          :color="snackbarColor"
+          class="text-center"
       >
         {{ snackbarText }}
       </v-snackbar>
@@ -43,9 +43,10 @@
 import CircleLoader from "@/components/CircleLoader";
 import LoginForm from "@/components/LoginForm";
 import SignupForm from "@/components/SignupForm";
+
 export default {
   name: "LoginAndSignUp",
-  components: { LoginForm, SignupForm, CircleLoader },
+  components: {LoginForm, SignupForm, CircleLoader},
   data() {
     return {
       snackbarColor: "",
@@ -59,7 +60,7 @@ export default {
     toggleLoadingState(state) {
       this.loading = state;
     },
-    handleSnackbarEvent({ text, color }) {
+    handleSnackbarEvent({text, color}) {
       this.turnOnSnackbar();
       this.setSnackbarText(text);
       this.setSnackbarColor(color);
@@ -75,6 +76,11 @@ export default {
       this.snackbarColor = color;
     },
   },
+  computed: {
+    mobile() {
+      return this.$vuetify.breakpoint.mobile;
+    }
+  }
 };
 </script>
 
