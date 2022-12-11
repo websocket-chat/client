420d7a046b947890d05fc026b25c68623035c307
metal-face <bhughes@evertz.com>
2022-12-11 07:01:44 -0500

    Successful connection established with CYMUI websocket. Ability to send and receive messages established.

--- src/views/Messenger.vue
+++ src/views/Messenger.vue
@@ -1,5 +1,8 @@
 <template>
-  <v-card height="80%" width="80%" color="primary">
+  <v-card height="80%" width="80%" color="">
+    <v-btn @click="sendMessage('balls')">
+      click
+    </v-btn>
 
   </v-card>
 </template>
@@ -7,7 +10,40 @@
 <script>
 export default {
   name: "Messenger",
+  data() {
+    return {
+      message: "",
+      connection: null,
+    }
+  },
+  methods: {
+    sendMessage(message) {
+      this.connection.send(JSON.stringify(message));
+    }
+  },
+  computed: {
+    sessionID() {
+      return this.$store.getters["user/sessionID"];
+    }
+  },
+  mounted() {
+    this.connection = new WebSocket("wss://chat.cmyui.xyz/ws");
 
+    this.connection.addEventListener("open", () => {
+      // login
+      console.log(this.sessionID);
+      this.connection.send(this.sessionID);
+    });
+
+    this.connection.addEventListener("message", (e) => {
+      console.log(e.data);
+    });
+
+    this.connection.addEventListener("close", (e) => {
+      e.preventDefault();
+      console.log(e);
+    })
+  }
 
 }
 </script>
